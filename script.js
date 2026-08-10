2fe99a465199ca3fb83de3a855a0c2a66b2e055e
shammagalua-arch <shammagalua@gmail.com>
2026-08-10 17:57:20 +0800

    Update API URL in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // https://script.google.com/macros/s/XXXXXXXX/exec
 
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbyB5DA3D8V_8ZvqhCWgeGx8OE1IyXFiRQIzleWHMXhVTSRwTqILpcUoMGRsD_jTxRbICg/exec";
+  "https://script.google.com/macros/s/AKfycbzkAPNo4lbeEhtHJIbKuGhnPHWfp1W2EgxNwRbJuNil-OSpNhbjzXCwwrOQwNdSbz_9wA/exec";
 
 
 // ==========================================
